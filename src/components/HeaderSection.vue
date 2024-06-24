79930f80fa7b03568d6007de21c445a4ecbbc284
Angga Widiarta <anggawidiarta55@gmail.com>
2024-06-24 16:59:49 +0800

    up

--- src/components/HeaderSection.vue
+++ src/components/HeaderSection.vue
@@ -2,7 +2,7 @@
 <template>
   <section class="w-full">
     <h1 class="text-3xl font-semibold tracking-wide hover:text-green-600 text-left">
-      Expensea Tracsker Project
+      Expense Tracker
     </h1>
   </section>
 </template>
