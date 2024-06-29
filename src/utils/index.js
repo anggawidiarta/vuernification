ee96744a52ea79b89a8adae6936573d60d7f9097
Angga Widiarta <anggawidiarta55@gmail.com>
2024-06-30 00:30:19 +0800

    chore: add utils function

---
+++ src/utils/index.js
@@ -0,0 +1,7 @@
+export const generateUniqueId = () => {
+  return Math.floor(Math.random() * 1000000000);
+};
+
+export const saveTransactionsToLocalStorage = (object) => {
+  localStorage.setItem('transactions', JSON.stringify(object));
+};
