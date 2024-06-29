45f8dae9e6b51d04d75508c10069c2c3e5b90f32
Angga Widiarta <anggawidiarta55@gmail.com>
2024-06-30 00:35:11 +0800

    fix: add incomeexpense feature

--- src/components/IncomeExpense.vue
+++ src/components/IncomeExpense.vue
@@ -5,12 +5,12 @@
   >
     <div class="flex flex-col items-center justify-center gap-2">
       <h3 class="font-bold">Income</h3>
-      <p class="text-green-500">$0.00</p>
+      <p class="text-green-500">${{ props.income }}</p>
     </div>
     <div class="border h-3/4 place-self-center"></div>
     <div class="flex flex-col items-center justify-center gap-2">
       <h3 class="font-bold">Expense</h3>
-      <p class="text-red-500">$0.00</p>
+      <p class="text-red-500">-${{ props.expense * -1.0 }}</p>
     </div>
   </section>
 </template>
