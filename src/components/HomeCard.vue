e8fc683e87999f3aeeb4ded0996a9b424748c28c
Angga Widiarta <anggawidiarta55@gmail.com>
2024-07-05 21:44:17 +0800

    feat: add homecard component

---
+++ src/components/HomeCard.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="mx-auto max-w-7xl">
+    <div class="container m-auto">
+      <div class="grid grid-cols-1 gap-4 p-4 md:grid-cols-2 place-items-center">
+        <div class="card glass w-96">
+          <figure>
+            <img
+              src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+              alt="car!"
+            />
+          </figure>
+          <div class="card-body">
+            <h2 class="card-title">For Tourist</h2>
+            <p>Browse our guides to learn more about trip</p>
+            <div class="justify-end card-actions">
+              <button class="btn btn-primary">Learn now!</button>
+            </div>
+          </div>
+        </div>
+
+        <div class="card glass w-96">
+          <figure>
+            <img
+              src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+              alt="car!"
+            />
+          </figure>
+          <div class="card-body">
+            <h2 class="card-title">For Guides</h2>
+            <p>List of trip package for tourist who want to travel</p>
+            <div class="justify-end card-actions">
+              <button class="btn btn-primary">Learn now!</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
