0cc09bc77f28e42b556e53cebd559ac61d582147
Angga Widiarta <anggawidiarta55@gmail.com>
2024-07-05 14:16:14 +0800

    chore: just test for style

---
+++ src/components/NavbarSection.vue
@@ -0,0 +1,13 @@
+<script setup></script>
+<template>
+  <nav class="w-full bg-black h-16">
+    <div
+      class="justify-end mx-auto items-center h-full gap-4 text-white max-w-7xl hidden md:flex px-3"
+    >
+      <div>HOme tseassd</div>
+      <div>HOme tseassd</div>
+      <div>HOme tseassd</div>
+      <div>HOme tseassd</div>
+    </div>
+  </nav>
+</template>
