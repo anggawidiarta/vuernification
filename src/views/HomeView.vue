41d1d72bc3cb79baeadc1286ff842ac659e6d986
Angga Widiarta <anggawidiarta55@gmail.com>
2024-07-05 21:44:57 +0800

    feat: add homecard to homeview

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,12 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+// import TheWelcome from "../components/TheWelcome.vue";
+import HeroSection from "@/components/HeroSection.vue";
+import HomeCard from "@/components/HomeCard.vue";
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <HeroSection />
+    <HomeCard />
   </main>
 </template>
