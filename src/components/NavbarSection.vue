08bd220747f779cb1c7741df2d2236729f8045a7
Angga Widiarta <anggawidiarta55@gmail.com>
2024-07-06 22:59:56 +0800

    style: fix style for navbar section

--- src/components/NavbarSection.vue
+++ src/components/NavbarSection.vue
@@ -1,13 +1,70 @@
-<script setup></script>
+<script setup>
+import { RouterLink, useRoute } from "vue-router";
+const isActiveLink = (routePath) => {
+  const route = useRoute();
+  return route.path === routePath;
+};
+</script>
+
 <template>
-  <nav class="w-full bg-black h-16">
-    <div
-      class="justify-end mx-auto items-center h-full gap-4 text-white max-w-7xl hidden md:flex px-3"
-    >
-      <div>HOme tseassd</div>
-      <div>HOme tseassd</div>
-      <div>HOme tseassd</div>
-      <div>HOme tseassd</div>
+  <nav class="w-full bg-black h-20">
+    <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+      <div class="flex h-20 items-center justify-between">
+        <div
+          class="flex flex-1 items-center justify-center md:items-stretch md:justify-start"
+        >
+          <!-- Logo -->
+          <RouterLink class="flex flex-shrink-0 items-center mr-4" to="/">
+            <img class="h-10 w-auto" :src="logo" alt="Vue Trips" />
+            <span class="hidden md:block text-white text-2xl font-bold ml-2"
+              >Vue Trips</span
+            >
+          </RouterLink>
+          <div class="md:ml-auto">
+            <div class="flex space-x-2">
+              <RouterLink
+                to="/"
+                :class="[
+                  isActiveLink('/')
+                    ? 'bg-green-900'
+                    : 'hover:bg-gray-900 hover:text-white',
+                  'text-white',
+                  'px-3',
+                  'py-2',
+                  'rounded-md',
+                ]"
+                >Home</RouterLink
+              >
+              <RouterLink
+                to="/trips"
+                :class="[
+                  isActiveLink('/trips')
+                    ? 'bg-green-900'
+                    : 'hover:bg-gray-900 hover:text-white',
+                  'text-white',
+                  'px-3',
+                  'py-2',
+                  'rounded-md',
+                ]"
+                >Trips</RouterLink
+              >
+              <RouterLink
+                to="/trips/add"
+                :class="[
+                  isActiveLink('/trips/add')
+                    ? 'bg-green-900'
+                    : 'hover:bg-gray-900 hover:text-white',
+                  'text-white',
+                  'px-3',
+                  'py-2',
+                  'rounded-md',
+                ]"
+                >Add Trip</RouterLink
+              >
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </nav>
 </template>
