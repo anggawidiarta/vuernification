9d9ce9c9038d0ebe744d722fbe6c4fe60566c1d7
Angga Widiarta <anggawidiarta55@gmail.com>
2024-07-06 22:57:50 +0800

    chore: tidy up

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <script setup>
-import { RouterLink, RouterView } from "vue-router";
-import HelloWorld from "./components/HelloWorld.vue";
+import { RouterView } from "vue-router";
 import NavbarSection from "./components/NavbarSection.vue";
 </script>
 
