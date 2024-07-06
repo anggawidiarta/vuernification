f6ea1bb911d43da17760534272aad419352710cd
Angga Widiarta <anggawidiarta55@gmail.com>
2024-07-06 22:59:20 +0800

    chore: add trip listings to homeview

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,11 +2,13 @@
 // import TheWelcome from "../components/TheWelcome.vue";
 import HeroSection from "@/components/HeroSection.vue";
 import HomeCard from "@/components/HomeCard.vue";
+import TripListings from "@/components/TripListings.vue";
 </script>
 
 <template>
   <main>
     <HeroSection />
     <HomeCard />
+    <TripListings />
   </main>
 </template>
